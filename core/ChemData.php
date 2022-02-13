39c3e7845755fcbfb15344e84297e565d06bac91
Dummdevka <87282400+Dummdevka@users.noreply.github.com>
2022-02-13 16:53:42 +0300

    ChemController added

---
+++ core/ChemData.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace core;
+
+class ChemData
+{
+    public $conn;
+
+    public function __construct() {
+        //Check if there is a connection
+            //Return the existing on
+        //Create new and assign
+    }
+    public function getDataByElement() {
+        
+    }
+    public function getDataByName() {
+
+    }
+    public function getDataByFormula() {
+
+    }
+
+}
