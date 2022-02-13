60bf39a57f934c8d662dde956aa16cfce22fa0e5
Dummdevka <87282400+Dummdevka@users.noreply.github.com>
2022-02-13 16:47:21 +0300

    Routes

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -1,10 +1,34 @@
 <?php
+
+declare (strict_types = 1);
+
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
+use Slim\Routing\RouteCollectorProxy;
 
-// declare (strict_types = 1);
-
-$app->get( '/', function( Request $request, Response $response, $args ) {
-    $response->getBody()->write( 'Hello' );
+$app->get('/', function( Request $request, Response $response, $args ) {
+    $response->getBody()->write( 'Welcome!' );
     return $response;
+});
+
+//Get info about a chemical element
+$app->group( '/data', function( RouteCollectorProxy $group ) {
+    $group->get('', function( Request $request, Response $response, $args ) {
+        $response->getBody()->write( 'Get element data!' );
+        return $response;
+    });
+    $group->get( '/element/{element}', function( Request $request, Response $response, $args ) {
+        $response->getBody()->write( 'Element info' );
+        return $response;
+    });
+    //Get info about a chemical element by its name
+    $group->get( '/name/{name}', function( Request $request, Response $response, $args ) {
+        $response->getBody()->write( 'Element info by name' );
+        return $response;
+    });
+    //Get info about a chemical element
+    $group->get( '/formula/{formula}', function( Request $request, Response $response, $args ) {
+        $response->getBody()->write( 'Formula info' );
+        return $response;
+    });
 });
